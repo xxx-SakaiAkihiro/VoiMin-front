142c2b8a53aa71ba24a0159e6e7c143e2c07fb0d
SakaiAkihiro <akihiro.sakai@rakus-partners.co.jp>
2020-06-17 15:25:03 +0900

    録音記録の更新機能作成

--- src/components/SaveDetail.vue
+++ src/components/SaveDetail.vue
@@ -15,19 +15,19 @@
       <v-row class="title" justify="center">{{result[0].date | moment}}</v-row>
       <v-row class="mt-3">
         <v-col>
-          <v-text-field label="Title" outlined rounded dense v-model="result[0].title"></v-text-field>
+          <v-text-field label="Title" outlined rounded dense v-model="param.title"></v-text-field>
         </v-col>
         <v-col>
-          <v-text-field label="Member" outlined rounded dense v-model="result[0].member"></v-text-field>
+          <v-text-field label="Member" outlined rounded dense v-model="param.member"></v-text-field>
         </v-col>
         <v-col>
-          <v-text-field label="Note" outlined rounded dense v-model="result[0].remarks"></v-text-field>
+          <v-text-field label="Note" outlined rounded dense v-model="param.remarks"></v-text-field>
         </v-col>
       </v-row>
-      <v-textarea outlined auto-grow label="Content" rows="14" v-model="result[0].content"></v-textarea>
+      <v-textarea outlined auto-grow label="Content" rows="14" v-model="param.content"></v-textarea>
 
       <v-col>
-        <v-btn rounded color="primary" absolute right>
+        <v-btn rounded color="primary" absolute right style="outline:0" @click="edit()">
           <v-icon class="mr-1">mdi-cached</v-icon>Update
         </v-btn>
       </v-col>
@@ -36,11 +36,19 @@
 </template>
 
 <script>
+import axios from "axios";
 import moment from "moment";
+
 export default {
   data() {
     return {
-      result: ""
+      result: "",
+      param: {
+        title: String,
+        member: String,
+        remarks: String,
+        content: String
+      }
     };
   },
   filters: {
@@ -58,11 +66,28 @@ export default {
       return this.$store.state.recordingList;
     }
   },
+  methods: {
+    edit() {
+      axios.post("/edit", {
+        title: this.param.title,
+        member: this.param.member,
+        remarks: this.param.remarks,
+        content: this.param.content,
+        recordingId: this.result[0].recordingId
+      });
+      alert("投稿内容の変更に成功しました。");
+      this.$router.push("/saveList");
+    }
+  },
   mounted() {
     /** 全議事録からIDの絞り込み */
     this.result = this.recordingList.filter(
       v => v.recordingId === this.rcordingData.rcordingId
     );
+    this.param.title = this.result[0].title;
+    this.param.member = this.result[0].member;
+    this.param.remarks = this.result[0].remarks;
+    this.param.content = this.result[0].content;
   }
 };
 </script>
