884bc740223993e06f4035db6d3cf65f51cc9767
ikedariho <riho.ikeda@rakus-partners.co.jp>
2020-06-17 15:40:24 +0900

    コンソールエラー改修

--- src/components/SaveList.vue
+++ src/components/SaveList.vue
@@ -7,23 +7,35 @@
           style="font-size:18px;font-weight:bold;"
           class="ma-5"
           v-if="dateFilterList.length === 0"
-        >保存されている録音記録はありません。</div>
+        >
+          保存されている録音記録はありません。
+        </div>
       </center>
       <v-list-item-group>
-        <v-list-itemtwo-line name="title" v-for="(item, i) in dateFilterList" :key="i">
+        <v-list
+          two-line
+          name="title"
+          v-for="(item, i) in dateFilterList"
+          :key="i"
+        >
           <v-list-item-content>
             <v-divider></v-divider>
-            <v-list-item-title v-text="item.date" id="dateTitle"></v-list-item-title>
+            <v-list-item-title
+              v-text="item.date"
+              id="dateTitle"
+            ></v-list-item-title>
             <v-list-item-subtitle v-for="(item, i) in item.itemList" :key="i">
               <v-list-item id="itemTitle">
                 <v-col cols="18" @click="detail(item)">{{ item.title }}</v-col>
                 <v-btn icon>
-                  <v-icon @click="recordingDelete(item.rcordingId)" color="grey">mdi-delete</v-icon>
+                  <v-icon @click="recordingDelete(item.rcordingId)" color="grey"
+                    >mdi-delete</v-icon
+                  >
                 </v-btn>
               </v-list-item>
             </v-list-item-subtitle>
           </v-list-item-content>
-        </v-list-itemtwo-line>
+        </v-list>
       </v-list-item-group>
     </v-list>
   </v-card>
@@ -37,7 +49,7 @@ export default {
   data() {
     return {
       getRecordingList: [],
-      recordingList: []
+      recordingList: [],
     };
   },
   methods: {
@@ -45,16 +57,16 @@ export default {
       if (confirm("削除してよろしいですか？")) {
         axios
           .post("/delete", {
-            recordingId: Id
+            recordingId: Id,
           })
           .then(() => {
             alert("削除しました");
             this.$router.go({
               path: this.$router.currentRoute.path,
-              force: true
+              force: true,
             });
           })
-          .catch(e => {
+          .catch((e) => {
             alert("問題が発生しました。もう1度作業をやり直してください。" + e),
               this.$router.push("/home");
           });
@@ -63,14 +75,14 @@ export default {
     detail(item) {
       this.$router.push({
         name: "SaveDetail",
-        query: { item: encodeURIComponent(JSON.stringify(item)) }
+        query: { item: encodeURIComponent(JSON.stringify(item)) },
       });
-    }
+    },
   },
   computed: {
     dateFilterList() {
       var duplicationDateMap = new Map(
-        this.recordingList.map(r => [r.date, r.date])
+        this.recordingList.map((r) => [r.date, r.date])
       );
       var duplicationDateList = Array.from(duplicationDateMap.values());
       var dateFilterList = [];
@@ -84,21 +96,21 @@ export default {
           if (dateFilterList[num].date === array.date) {
             var filterRecordingObject = {
               title: array.title,
-              rcordingId: array.recordingId
+              rcordingId: array.recordingId,
             };
             dateFilterList[num].itemList.push(filterRecordingObject);
           }
         });
       }
       return dateFilterList;
-    }
+    },
   },
   created() {
     axios
       .post("/findRecording", {
-        userId: this.$store.state.loginUser.userId
+        userId: this.$store.state.loginUser.userId,
       })
-      .then(response => {
+      .then((response) => {
         this.getRecordingList = response.data;
         this.$store.dispatch("setRecordingList", this.getRecordingList);
 
@@ -109,11 +121,11 @@ export default {
             member: this.getRecordingList[num].member,
             remarks: this.getRecordingList[num].remarks,
             title: this.getRecordingList[num].title,
-            recordingId: this.getRecordingList[num].recordingId
+            recordingId: this.getRecordingList[num].recordingId,
           });
         }
       });
-  }
+  },
 };
 </script>
 
